c2551467334aeec6c5226dd5f5fffa9aec5a67a5
Omar Valikhanov <omar.velikhanov@gmail.com>
2020-05-04 13:05:12 +0400

    Fixed markup of account screen

--- module-5-team-2/src/account/AccountRowElement.jsx
+++ module-5-team-2/src/account/AccountRowElement.jsx
@@ -1,54 +1,54 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {
-  DecPartSpan,
-  RowContainer,
-  Row,
-  RowElemTickerDiv,
-  RowElemNameDiv,
-  RowElemAmountDiv,
-  RowElemSumDiv,
-  RowElemProfitDiv,
+    DecPartSpan,
+    RowContainer,
+    Row,
+    RowElemTickerDiv,
+    RowElemNameDiv,
+    RowElemAmountDiv,
+    RowElemSumDiv,
+    RowElemProfitDiv,
 } from "./styleRowElement";
 import {NavLink} from "react-router-dom";
 
 class RowElement extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
 
-  splitDecimals = (number) => {
-    const [wholePart, decPart] = number.toString().split(".");
-    return (
-      <>
-        {wholePart}.<DecPartSpan>{decPart} $</DecPartSpan>
-      </>
-    );
-  };
+    splitDecimals = (number) => {
+        const [wholePart, decPart] = number.toString().split(".");
+        return (
+            <>
+                {wholePart}.<DecPartSpan>{decPart} $</DecPartSpan>
+            </>
+        );
+    };
 
-  render() {
-    const { code, name, amount, purchasePrice, profit, sign } = this.props;
-    return (
-      <RowContainer>
-        <NavLink style={{textDecoration: 'none'}} to={"/sell/" + code}>
-          <Row>
-            {" "}
-            <RowElemTickerDiv>{code}</RowElemTickerDiv>
-            <RowElemNameDiv>
-              {name === "undefined" || !name ? <i>{code}</i> : name}
-            </RowElemNameDiv>
-            <RowElemAmountDiv>{amount} pcs</RowElemAmountDiv>
-            <RowElemSumDiv>
-              {this.splitDecimals((amount * purchasePrice).toFixed(3))}
-            </RowElemSumDiv>
-            <RowElemProfitDiv isNegative={sign < 0 ? true : false}>
-              {profit}
-            </RowElemProfitDiv>
-          </Row>
-        </NavLink>
-      </RowContainer>
-    );
-  }
+    render() {
+        const {code, name, amount, purchasePrice, profit, sign} = this.props;
+        return (
+            <NavLink style={{textDecoration: 'none', color: 'inherit'}} to={"/sell/" + code}>
+                <RowContainer>
+                    <Row>
+                        {" "}
+                        <RowElemTickerDiv>{code}</RowElemTickerDiv>
+                        <RowElemNameDiv>
+                            {name === "undefined" || !name ? <i>{code}</i> : name}
+                        </RowElemNameDiv>
+                        <RowElemAmountDiv>{amount} pcs</RowElemAmountDiv>
+                        <RowElemSumDiv>
+                            {this.splitDecimals((amount * purchasePrice).toFixed(3))}
+                        </RowElemSumDiv>
+                        <RowElemProfitDiv isNegative={sign < 0 ? true : false}>
+                            {profit}
+                        </RowElemProfitDiv>
+                    </Row>
+                </RowContainer>
+            </NavLink>
+        );
+    }
 }
 
 export default RowElement;
